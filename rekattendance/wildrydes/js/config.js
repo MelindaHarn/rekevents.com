2fc383190c4073ebce441faca11cf30643037bdd
MelindaHarn <50530586+MelindaHarn@users.noreply.github.com>
2019-05-12 03:50:57 -0400

    Create config.js

---
+++ rekattendance/wildrydes/js/config.js
@@ -0,0 +1,21 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_lIA1D8mqe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '64snnshifjn525hmoh1c6agqlm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://b7mtti30vi.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+   }
+};
+
+//window._config = {
+//    cognito: {
+//        userPoolId: 'us-east-1_NIXKwdWvN', // e.g. us-east-2_uXboG5pAb
+//        region: 'us-east-1', // e.g. us-east-2
+//		userPoolClientId: '6gnnnk809q5paeo5a0l935mkei' //is this used anywhere?
+//    },
+//    api: {
+//        invokeUrl: 'https://g33gt6copl.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+//    }
+//};
